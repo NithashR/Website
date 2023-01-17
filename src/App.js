5ab536de69b92d89727d7f633b481f5da511f694
NithashR <nithash.rajendram@ryerson.ca>
2023-01-16 21:44:45 -0500

    c

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} exact />
+        <Route path="/resume/" element={<Home/>} exact />
       </Routes>
     </Router>
   );
